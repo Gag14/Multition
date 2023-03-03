2704a1ed418a887cc762fa17e5a192f3b59d820e
Gag14 <121533631+Gag14@users.noreply.github.com>
2023-03-03 15:48:37 +0400

    Add files via upload

---
+++ Singlton.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+class Multiton 
+{
+	public:
+		static Multiton* Instance();
+		static int i;
+		~Multiton();
+	protected:
+		Multiton();
+	private:
+		static Multiton* _instance;
+
+		static Multiton* _instance2;
+};
+int Multiton::i = 0;
+Multiton::Multiton() 
+{
+	std::cout << "I am in here" << std::endl;
+}
+Multiton::~Multiton() 
+{
+	delete _instance;
+	delete _instance2;
+	_instance = nullptr;
+	_instance2 = nullptr;
+}
+Multiton* Multiton::_instance = 0;
+Multiton* Multiton::_instance2 = 0;
+Multiton* Multiton::Instance () 
+{
+	if (i == 0) 
+	{
+		_instance = new Multiton;
+		std::cout << "Instance 1" << std::endl;
+	}
+	
+	if (i == 1) 
+	{
+		_instance2 = new Multiton;
+		std::cout << "Instance 2" << std::endl; 
+	}
+	++i;
+	if (i % 2 == 0) {std::cout << "Now returning instance 2 " << std::endl; return _instance2; }
+	std::cout << "Returning Instance 1" << std::endl;
+	return _instance;
+}
+int main () 
+{
+	Multiton* ptr = Multiton::Instance();
+	Multiton* str = Multiton::Instance();
+	bool a = (ptr == str);
+	std::cout << a << std::endl;
+	
+	Multiton* ptr1 = Multiton::Instance();
+	Multiton* str2 = Multiton::Instance();
+	bool b = ptr1 == str2;
+	std::cout << b << std::endl;
+return 0;
+}
